8e74ef2d7c598395df91f2c3b7d34d63b3a8a1ff
doggie52@gmail.com <doggie52@gmail.com@03a429be-ee9c-878f-3e3c-cef132489739>
2012-04-10 20:46:45 +0000

    Initial Android project structure.

---
+++ src/com/barnacle/game/BarnacleGameActivity.java
@@ -0,0 +1,15 @@
+package com.barnacle.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BarnacleGameActivity extends Activity
+{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.main );
+	}
+}
